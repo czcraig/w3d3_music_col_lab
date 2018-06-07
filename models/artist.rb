6334a3c2b645e6739d7d674c0556eebf77261bdd
Chris Craig <ctc2@live.co.uk>
2018-06-07 09:51:44 +0100

    changed [0] to .first()

--- models/artist.rb
+++ models/artist.rb
@@ -20,7 +20,7 @@ def save()
   sql = "INSERT INTO artists (name) VALUES ($1) RETURNING id"
   values = [@name]
   result = SqlRunner.run(sql, values)
-  @id = result[0]["id"].to_i
+  @id = result.first()["id"].to_i
 end
 
 
